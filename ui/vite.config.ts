1f181fb35c054666b42d2aaa78ab47276cf15da4
Hao Wang <hao.1.wang@gmail.com>
2026-04-04 18:05:32 -0400

    Updates

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,6 +4,7 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  cacheDir: '/app/.vite-cache',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
