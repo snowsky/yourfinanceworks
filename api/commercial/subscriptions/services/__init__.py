f898464ff6ce76ec36eef6c88eb7252924cf81f8
Hao Wang <hao.1.wang@gmail.com>
2026-06-14 15:05:33 -0400

    feat(subscriptions): expose needs_review on list + per-row responses

--- api/commercial/subscriptions/services/__init__.py
+++ api/commercial/subscriptions/services/__init__.py
@@ -11,6 +11,11 @@ from commercial.subscriptions.services.subscription_query import (
     set_cancel_reminder,
     acknowledge_price_change,
 )
+from commercial.subscriptions.services.subscription_review import (
+    build_summary,
+    evaluate_review,
+    to_response,
+)
 
 __all__ = [
     "ScanResult",
@@ -20,4 +25,7 @@ __all__ = [
     "update_status",
     "set_cancel_reminder",
     "acknowledge_price_change",
+    "build_summary",
+    "evaluate_review",
+    "to_response",
 ]
